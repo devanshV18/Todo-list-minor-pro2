6ef6aa7455da2c0ffd9861c10e08750069eafd77
devanshV18 <devanshverma024@gmail.com>
2023-11-12 00:59:21 +0530

    definign fxns

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { TodoProvider } from './contexts'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  //the state todos stores  all the todos in the presc obj structure as it is a array and can store an array of strings
+  const [todos,setTodos] = useState([])
+
+  //The todo argument is a string coming from the user input filled form
+  const addTodo = (todo) => {
+    //VVVIII--- setTodos(todo) here we are storing the todo from user in the state (array) but writing like this deletes all previous entries and enters the current only which is not to be done
+    //injecting todo as its obj structure:-
+    setTodos((prev) => [{id:Date.now(), ...todo},...prev]) 
+  }
+
+
+  const updatedTodo = (id , todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
 
   return (
-    <>
-      
-    </>
+    //todos variable accessed by the provider the same array(containing obj which is our todo) with todos and its structure defined in context 
+    <TodoProvider value = {{todos,addTodo,updatedTodo,deleteTodo,toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
   )
 }
 
--- TodoList/src/contexts/TodoContext.js
+++ TodoList/src/contexts/TodoContext.js
@@ -1,6 +1,22 @@
 import { createContext, useContext } from "react";
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+   //creating a default structure of TODO
+    todos: [
+        {
+            id:1,
+            todo: "todo msg",
+            completed: flase
+        },
+        //there will be multiple and basically all todos here with the above structure accessible for usag eby functionalities and componenets`
+        
+    ],
+    //todo will be the input msg and subsequent function is invoked
+    addTodo: (todo) => {} ,
+    updatedTodo: (id,todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
 
 export const useTodo = () => {
     return useContext(TodoContext)
